716df01f226d72ce818166433ebe52372ee43dcc
the-hobbes <phelan.vendeville@gmail.com>
2013-04-08 18:50:02 -0400

    initial commit

---
+++ oldstuff/thanks.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="http://www.uvm.edu/www/css/uvm_auto_style.css" type="text/css" media="Screen" />
+	<link rel="stylesheet" href="/it/includes/style/stylesheet.php" type="text/css" media="all" />
+	<link rel="stylesheet" type="text/css" href="/www/css/uvm2010print.css" media="print" />
+	<script type="text/javascript" src="/www/js/modernizr-2.0.6.js"></script>	
+	<script type="text/javascript" src="/www/js/jquery-1.6.4.min.js"></script>		
+	<script type="text/javascript" src="/www/js/jquery-ui-1.8.16.custom.min.js"></script>
+	<meta name="author" content="University of Vermont" />
+<meta name="generator" content="UVM Web Publishing System" />
+		<link rel="shortcut icon" href="http://www.uvm.edu/www/images/templates/favicon.ico" />
+	<base href="http://www.uvm.edu/it/help/" />
+	<title>Help & Support : University of Vermont</title>
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-4758296-1']);
+		_gaq.push(['_setDomainName', 'none']);
+		_gaq.push(['_setAllowLinker', true]);
+		_gaq.push(['_trackPageview']);
+		_gaq.push(['_trackPageLoadTime']);	
+	 	_gaq.push(['b._setAccount', 'UA-10987118-1']);
+		_gaq.push(['b._setDomainName', 'none']);
+		_gaq.push(['b._setAllowLinker', true]);
+		_gaq.push(['b._trackPageview']);
+		_gaq.push(['b._trackPageLoadTime']);	
+		(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; 
+		s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+	<!-- Google Analytics Social Button Tracking -->
+	<script type="text/javascript" src="https://s3.amazonaws.com/ha_js/ga_social_tracking.js"></script>
+
+	<style type="text/css">
+		#errors
+		{
+			width:250px;
+			height:auto;
+			position:absolute;
+			top:50%;
+			right:15%;
+			border:1px solid red;
+			background-color:white;
+			text-align:left;
+			padding:5px;
+			display:none;
+		}
+	</style>
+	<meta http-equiv="Refresh" content="3; url= http://www.uvm.edu/~cdclinic/CDCsignin.php" />
+</head>
+<body>
+<div class="printOnly"><img src="/www/images/logos/printhead-10.gif" alt="" width="250" height="30" /></div>
+<div id="mainframe">
+<div id="skip">
+	<a href="#mainnavlist" onclick="_gaq.push(['_trackEvent', 'Links', 'Accessibility', 'Skip to nav']);">Skip to local navigation</a>
+	<a href="#uvmmaincontent" onclick="_gaq.push(['_trackEvent', 'Links', 'Accessibility', 'Skip to content']);">Skip to main content</a>
+</div>
+<div id="uvmtools">
+<div id="uvmtoolbar">
+<ul>
+	<li class="az"><a href="/a_to_z/" title="alphabetical listing of departments, organizations and key areas ">A-Z</a></li>
+	<li><a href="/directory/" title="searchable directory of university community members">Directory</a></li>
+	<li><a href="https://myuvm.uvm.edu/cp/home/displaylogin" title="Log into the myUVM portal" onclick="_gaq.push(['_trackEvent', 'Links', 'Internal', 'myUVM']);">myUVM</a></li>
+</ul>
+<div id="cse-search-form">
+<noscript>
+<form action="http://www.google.com/cse" id="cse-search-box">
+	<input type="hidden" name="cx" value="009525375878856613939:vxps9xejxke" />
+	<input type="hidden" name="ie" value="UTF-8" />
+	<input type="hidden" name="nojs" value="1" />
+	<input type="text" name="q" size="24" />
+	<input type="submit" name="sa" value="Search" />
+</form>
+</noscript>
+</div>
+</div>
+</div>
+<a href="/"><img src="/www/images/templates/tower2010.gif" alt="" id="uvmtower" /></a>
+<div id="uvmlogo">
+<h1><a href="/"><img src="/www/images/templates/uvmlogo-words.gif" alt="The University of Vermont" /></a></h1>
+<div class="uvmapply"><a href="/admissions/" title="Apply to UVM as an undergraduate" onclick="_gaq.push(['_trackEvent', 'Links', 'Internal', 'Apply to UVM']);">Apply to <abbr>UVM</abbr></a></div>
+</div>
+<div id="uvmnavigation">
+<ul class="topnav">
+	<li><a href="/about_uvm/" class="mm_about_uvm">About UVM</a><noscript><span></span></noscript>
+		<ul class="subnav right">
+				<li><a href="/about_uvm/?Page=stories/stories.php">UVM Stories</a></li>
+				<li><a href="/about_uvm/?Page=burlington/burlington.html&amp;SM=burlingtonsubmenu.html">Burlington, Vermont</a></li>
+				<li><a href="/about_uvm/?Page=history/default.html&amp;SM=historysubmenu.html">History and Traditions</a></li>
+				<li><a href="/about_uvm/visiting/">Visiting, Touring, Getting Around</a></li>
+				<li><a href="/about_uvm/?Page=governance.html">UVM Governance</a></li>
+				<li><a href="/about_uvm/?Page=facts.html">Fast Facts</a></li>
+		</ul>
+	</li>
+	<li><a href="/admissions/" class="mm_admissions">Admissions</a><noscript><span></span></noscript>
+		<ul class="subnav right">
+				<li><a href="/admissions/undergraduate/">Undergraduate Admissions</a></li>
+				<li><a href="/admissions/?Page=graduate_admissions.html">Graduate Admissions</a></li>
+				<li><a href="/admissions/?Page=com_admissions.html">College of Medicine</a></li>
+				<li><a href="/admissions/?Page=ce_admissions.html">Continuing Education</a></li>
+		</ul>
+	</li>
+	<li><a href="/academics/" class="mm_academics">Academics</a><noscript><span></span></noscript>
+		<ul class="subnav right">
+				<li><a href="/academics/?Page=programs_degrees.php">Majors, Minors and Graduate Programs </a></li>
+				<li><a href="/academics/?Page=colleges.php">Schools and Colleges </a></li>
+				<li><a href="/academics/?Page=lifelonglearning.html">Lifelong Learning </a></li>
+				<li><a href="/academics/?Page=libraries.html">Libraries and Technology </a></li>
+				<li><a href="/academics/?Page=courses_reg.html">Course and Registration Information</a></li>
+				<li><a href="/academics/?Page=http://www.uvm.edu/~rgweb/importantdates/i_academiccalendars.html">Academic Calendar</a></li>
+				<li><a href="/academics/?Page=services.php">Academic Services and Support</a></li>
+		</ul>
+	</li>
+	<li><a href="/student_life/" class="mm_student_life">Student Life</a><noscript><span></span></noscript>
+		<ul class="subnav right">
+				<li><a href="/student_life/?Page=involved.php">Getting Involved</a></li>
+				<li><a href="/student_life/?Page=fitness.html">Sports and Fitness </a></li>
+				<li><a href="/student_life/?Page=wellbeing.html">Well-Being</a></li>
+				<li><a href="/student_life/?Page=housing.html">Housing</a></li>
+				<li><a href="/student_life/?Page=dining.php">Dining</a></li>
+				<li><a href="/student_life/?Page=diversity.html">Diversity on Campus</a></li>
+				<li><a href="/student_life/?Page=arts.html">The Arts </a></li>
+				<li><a href="/student_life/?Page=http://www.uvm.edu/academics/services.php">Academic Services and Support </a></li>
+				<li><a href="/student_life/?Page=hub.html">Social Media at UVM</a></li>
+		</ul>
+	</li>
+	<li><a href="/research/" class="mm_research">Research</a><noscript><span></span></noscript>
+		<ul class="subnav right">
+				<li><a href="/research/?Page=contact.html">Contact Us </a></li>
+		</ul>
+	</li>
+	<li><a href="/athleticsbridge/" class="mm_athleticsbridge">Athletics</a><noscript><span></span></noscript>
+		<ul class="subnav right">
+				<li><a href="/athleticsbridge/?Page=varsity.html">Varsity Athletics</a></li>
+				<li><a href="/athleticsbridge/?Page=clubsports.html">Club Sports</a></li>
+				<li><a href="/athleticsbridge/?Page=rec.html">Intramurals and Campus Recreation</a></li>
+				<li><a href="/athleticsbridge/?Page=facilities.html">Athletic Facilities</a></li>
+		</ul>
+	</li>
+	<li><a href="/offices/" class="mm_offices">Offices</a><noscript><span></span></noscript>
+		<ul class="subnav right">
+				<li><a href="/offices/?Page=governance.html">UVM Governance</a></li>
+		</ul>
+	</li>
+	<li><a href="/outreach/" class="mm_outreach">Outreach</a><noscript><span></span></noscript>
+		<ul class="subnav right">
+				<li><a href="/outreach/?Page=engagement.html">Community Engagement </a></li>
+				<li><a href="/outreach/?Page=impact.html">Economic Impact </a></li>
+				<li><a href="/outreach/?Page=business.php">Business and Industry Resources</a></li>
+		</ul>
+	</li>
+</ul></div>
+<div id="uvmcontent">
+ 
+<div id="uvmmainmenu">
+		<ul id="mainnavlist">
+		<li class="home"><a href="/">UVM Home</a></li>
+		<li class="location"><a href="../">Information Technology</a></li>
+		<li><a href="http://www.uvm.edu/it/help/">Help & Support</a>
+			<div class="selected"><ul><li><a href="http://www.uvm.edu/it/computers/">Computers &amp; Phones</a></li><li><a href="http://www.uvm.edu/it/email/">Email &amp; Messaging</a></li><li><a href="http://www.uvm.edu/it/network/">Network &amp; Internet</a></li><li><a href="http://www.uvm.edu/it/software/">Software &amp; Utilities</a></li><li><a href="http://www.uvm.edu/it/account/">NetID &amp; Account Management</a></li><li><a href="http://www.uvm.edu/it/security/">Information Security</a></li><li><a href="http://www.uvm.edu/it/policies/">Policies &amp; Compliance</a></li><li><a href="http://www.uvm.edu/it/contact/">Contact &amp; About ETS</a></li></ul></div>		</li>
+	</ul>
+</div>
+<div id="uvmcustombanner2"><div id="secondary-masthead">
+<h2>Information Technology</h2>
+</div>
+</div><div id="uvmcustomnav">
+<div id="global_navigation">
+<ul>
+	<li><strong>Gateways:</strong></li>
+	<li id="gate_student"><a href="/it/students/">Students</a></li>
+	<li id="gate_facstaff"><a href="/it/faculty/">Faculty &amp; Staff</a></li>
+	<li id="gate_coll-dept"><a href="/it/colleges/">Colleges &amp; Departments</a></li>
+	<li id="gate_alum-emerit-visit"><a href="/it/alumni/">Alumni, Emeriti &amp; Visitors</a></li>
+	<li id="gate_itpro" class="last"><a href="/it/itpro/">IT Professionals</a></li>
+</ul>
+</div>
+</div><div id="uvmmaincontent" class="uvmpage_CDCsignin_html">
+<!-- $the_magic_page = CDCsignin.html --><!-- $the_magic_action = Array --><h2>Computer Depot Clinic Sign-In</h2>
+ 
+<h2 style="color:red;"> Thank you for your submission. Please wait until the next available technician calls your name.</h2>
+
+<p class="modify">Last modified January 12 2012 03:55 PM</p></div>
+<div id="uvmfooter">
+<address>
+<a href="/talk_to_us/?email=Information.Technology%40uvm.edu&amp;what=Help+%26+Support" id="contactus">Contact UVM</a> &copy; 2012  The University of Vermont - Burlington, VT 05405 - (802) 656-3131
+</address></div>
+</div>
+</div>
+<script type="text/javascript" src="/www/js/dropdownmenus.js"></script>
+<script src="//www.google.com/jsapi" type="text/javascript"></script>
+<script src="/www/js/googlecse.js" type="text/javascript"></script>
+</body>
+</html>
---
+++ thanks.php
@@ -0,0 +1,2 @@
+<h2 style="color:red;"> Thank you for your submission. Please wait until the next available technician calls your name.</h2>
+	<meta http-equiv="Refresh" content="3; url=CDCsignin.php" />
